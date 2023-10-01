973c332f6da8197c6ce765d5b16b6c7b56e1973c
AndrewSanych <andrich0105@gmail.com>
2023-10-01 20:06:17 +0300

    Add project files.

---
+++ 0.1_Messenger_Client/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _0._1_Messenger_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int port = 8080;
+        static string address = "127.0.0.1";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        UdpClient client;
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            //port = int.Parse(PortTBox.Text);
+            //address = IPAddress.Parse(IpTBox.Text);
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+            IPEndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            client = new UdpClient();
+            try
+            {
+
+                string message = string.Empty;
+                while (true)
+                {
+                    // Message :
+                    message = MessageTBox.Text;
+                    MessageTBox.Text = string.Empty;
+
+                    if (message != string.Empty)
+                    {
+                        byte[] data = Encoding.Unicode.GetBytes(message);
+
+                        MessagesLBox.Items.Add("You : ");
+                        MessagesLBox.Items.Add(message);
+                        MessagesLBox.Items.Add("");
+
+                        // send :
+                        client.Send(data, data.Length, ipPoint);
+
+                        // Receive answer :
+                        data = client.Receive(ref remoteIpPoint);
+                        string response = Encoding.Unicode.GetString(data);
+
+                        MessagesLBox.Items.Add("Chat AI : ");
+                        MessagesLBox.Items.Add(response);
+                        MessagesLBox.Items.Add("");
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void EndChat_Click(object sender, RoutedEventArgs e)
+        {
+            // Close socket
+            client.Close();
+            this.Close();
+        }
+    }
+}
---
+++ Network_Programing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Network_Programing
+{
+    internal class Program
+    {
+        static int port = 8080;
+        static string address = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+            EndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            Console.WriteLine("Endter the message: ");
+            string message = "";
+            message = Console.ReadLine();
+            byte[] data = Encoding.Unicode.GetBytes(message);
+
+            socket.SendTo(data, ipPoint);
+        }
+    }
+}
